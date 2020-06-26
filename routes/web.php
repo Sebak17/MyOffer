2dd11991ebea727a9303508592d2c320ac761782
Sebak17 <sebakrysa@wp.pl>
2020-06-27 00:40:11 +0200

    New routes

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@main')->name('home');
 
+Route::get('/oferta/{id}/{name}', 'HomeController@offer')->where('id', '[0-9]+')->name('pageOfferItem');
+Route::get('/oferty', 'HomeController@offers')->name('pageOffersList');
 
-
-Route::get('/rejestracja', 'HomeController@main')->name('page_auth_register');
+Route::get('/rejestracja', 'HomeController@main')->name('pageAuthRegister');
 Route::any('/wyloguj', 'AuthorizationController@logout')->name('logout');
+
+
+Route::prefix('panel')->middleware('auth:web')->group(function () {
+
+    Route::get('/dodaj_ogloszenie', 'PanelController@offer_add')->name('pagePanelOfferAdd');
+
+    Route::get('/moje_ogloszenia', 'PanelController@offers_list')->name('pagePanelOffersList');
+
+    Route::get('/ustawienia', 'PanelController@settings')->name('pagePanelSettings');
+});
 
 Route::prefix('system')->group(function () {
 
 	Route::prefix('auth')->group(function () {
 		Route::post('signIn', 'AuthorizationController@signIn');
     	Route::post('signUp', 'AuthorizationController@signUp');
+	});;
+
+	Route::prefix('panel')->middleware('auth:web')->group(function () {
+		Route::post('offerImageUpload', 'PanelSystem\OffersController@offerImageUpload');
+		Route::post('offerImageRemove', 'PanelSystem\OffersController@offerImageRemove');
+		Route::post('offerAdd', 'PanelSystem\OffersController@addOffer');
 	});;
 });
